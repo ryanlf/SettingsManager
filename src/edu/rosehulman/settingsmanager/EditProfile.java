1acb17c485703c233aeeb8fb355b80adf930963a
mccammjr <mccammjr@rose-hulman.edu>
2013-10-28 10:04:07 -0400

    Added some more functionality to stubs and began work on sound popups

--- src/edu/rosehulman/settingsmanager/EditProfile.java
+++ src/edu/rosehulman/settingsmanager/EditProfile.java
@@ -1,10 +1,14 @@
 package edu.rosehulman.settingsmanager;
 
-import android.media.AudioManager;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +19,10 @@ import android.widget.SeekBar;
 public class EditProfile extends Activity {
 	private Profile mProfile;
 	private SeekBar mSystemVolumeControl;
+	private SeekBar mRingerVolumeControl;
+	private SeekBar mNotificationVolumeControl;
+	private Button mNotificationSoundButton;
+	private SeekBar mAlarmVolumeControl;
 	private AudioManager mAudioManager;
 	private Button mSaveButton;
 	private Button mCancelButton;
@@ -30,11 +38,17 @@ public class EditProfile extends Activity {
 		mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
 		requestCode = intent.getIntExtra(Profiles.KEY_REQUEST_CODE, 1);
 		//Identify controls
-		mSystemVolumeControl = (SeekBar) findViewById(R.id.system_volume_control);
 		mNameView = (EditText) findViewById(R.id.editText1);
+		mSystemVolumeControl = (SeekBar) findViewById(R.id.system_volume_control);
+		mRingerVolumeControl = (SeekBar) findViewById(R.id.ring_volume_control);
+		mNotificationVolumeControl = (SeekBar) findViewById(R.id.notification_volume_control);
+		mNotificationSoundButton = (Button) findViewById(R.id.notification_sound_spinner);
+		mAlarmVolumeControl = (SeekBar) findViewById(R.id.alarm_volume_control);
 		
-		if (requestCode == Profiles.REQUEST_CODE_EDIT_PROFILE) intializeEditProfile();
-		else intializeNewProfile();
+		if (requestCode == Profiles.REQUEST_CODE_EDIT_PROFILE)
+			intializeEditProfile();
+		else
+			intializeNewProfile();
 		
 		//Initialize buttons
 		mSaveButton = (Button) findViewById(R.id.save_button);
@@ -60,7 +74,10 @@ public class EditProfile extends Activity {
 		return true;
 	}
 	private void save(){
-		mProfile.setVolume(mSystemVolumeControl.getProgress());
+		mProfile.setSystemVolume(mSystemVolumeControl.getProgress());
+		mProfile.setRingerVolume(mRingerVolumeControl.getProgress());
+		mProfile.setNotificationVolume(mNotificationVolumeControl.getProgress());
+		mProfile.setAlarmVolume(mAlarmVolumeControl.getProgress());
 		mProfile.setName(mNameView.getText().toString());
 		Intent result = new Intent();
 		result.putExtra(Profiles.KEY_EDIT_PROFILE, mProfile);
@@ -72,15 +89,52 @@ public class EditProfile extends Activity {
 		this.finish();
 	}
 	private void intializeNewProfile(){
+		initializeBasicProfileSettings();
 		mSystemVolumeControl.setProgress(mAudioManager.getStreamVolume(AudioManager.STREAM_SYSTEM));
-		mSystemVolumeControl.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM));
-		mNameView.setActivated(false);
+		mRingerVolumeControl.setProgress(mAudioManager.getStreamVolume(AudioManager.STREAM_RING));
+		mNotificationVolumeControl.setProgress(mAudioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION));
+		mAlarmVolumeControl.setProgress(mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM));
+		updateNotificationButton(null);
 	}
 	private void intializeEditProfile(){
+		initializeBasicProfileSettings();
 		mSystemVolumeControl.setProgress(mProfile.getSystemVolume());
-		mSystemVolumeControl.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM));
+		mRingerVolumeControl.setProgress(mProfile.getRingerVolume());
+		mNotificationVolumeControl.setProgress(mProfile.getNotificationVolume());
+		mNotificationSoundButton.setText(mProfile.getNotificationRingtone());
+		mAlarmVolumeControl.setProgress(mProfile.getAlarmVolume());
 		mNameView.setText(mProfile.toString());
+	}
+	
+	private void initializeBasicProfileSettings(){
+		mSystemVolumeControl.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM));
+		mRingerVolumeControl.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_RING));
+		mNotificationVolumeControl.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION));
+		mAlarmVolumeControl.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM));
+		mNotificationSoundButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, false);
+				updateNotificationButton(intent);
+				
+				ME.startActivityForResult(intent, 5);
+			}
+		});
 		mNameView.setActivated(false);
 	}
+	
+	private void updateNotificationButton(Intent intent){
+		Uri current = RingtoneManager.getActualDefaultRingtoneUri(this, RingtoneManager.TYPE_NOTIFICATION);
+		Ringtone ringtone = RingtoneManager.getRingtone(this, current);
+		mNotificationSoundButton.setText(ringtone.getTitle(this));
+		if(intent != null)
+			intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, current);
+	}
+	
+	private EditProfile ME = this;
 
 }
--- src/edu/rosehulman/settingsmanager/Profile.java
+++ src/edu/rosehulman/settingsmanager/Profile.java
@@ -10,15 +10,18 @@ public class Profile implements Serializable {
 	
 	private String profileName;
 	private int systemVolume;
+	private int ringerVolume;
+	private int notificationVolume;
+	private String notificationRingtone;
+	private int alarmVolume;
 	
 	public Profile(){
 		profileName = null;
 		systemVolume = 0;
-	}
-	
-	public Profile(String profileName,int systemVolume) {
-		this.profileName = profileName;
-		this.systemVolume = systemVolume;
+		ringerVolume = 0;
+		notificationVolume = 0;
+		notificationRingtone = "";
+		alarmVolume = 0;
 	}
 	
 	@Override
@@ -26,22 +29,36 @@ public class Profile implements Serializable {
 		return profileName;
 	}
 	
-	public int getSystemVolume(){
-		return systemVolume;
-	}
+	public int getSystemVolume(){ return systemVolume; }
 
-	public void setVolume(int progress) {
-		systemVolume = progress;
-		
-	}
+	public void setSystemVolume(int progress) {	systemVolume = progress; }
+	
+	public int getRingerVolume(){ return ringerVolume; }
+
+	public void setRingerVolume(int progress) {	ringerVolume = progress; }
+	
+	public int getNotificationVolume(){	return notificationVolume; }
+
+	public void setNotificationVolume(int progress) { notificationVolume = progress; }
+
+	public String getNotificationRingtone(){	return notificationRingtone; }
+
+	public void setNotificationRingtone(String ringtone) { notificationRingtone = ringtone; }
+	
+	public int getAlarmVolume(){ return alarmVolume; }
+
+	public void setAlarmVolume(int progress) { alarmVolume = progress; }
 
 	public void setName(String string) {
 		profileName = string;
-		
 	}
 	public void setSettings(Activity parent){
 		AudioManager mAudioManager=(AudioManager) parent.getSystemService(Context.AUDIO_SERVICE);
 		mAudioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, systemVolume, 0);
+		mAudioManager.setStreamVolume(AudioManager.STREAM_RING, ringerVolume, 0);
+		mAudioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, notificationVolume, 0);
+		mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM, alarmVolume, 0);
+		
 		
 		
 	}
